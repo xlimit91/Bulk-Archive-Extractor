a466365981b65a3a583f630c1a283e7e0bd9d13b
Alex9303 <alexander.figart@gmail.com>
2016-12-17 09:58:39 -0500

    Allowed rar and 7z format

--- bulk zip extractor/Form1.Designer.vb
+++ bulk zip extractor/Form1.Designer.vb
@@ -29,6 +29,17 @@ Partial Class Form1
         Me.pnl_bar_line = New System.Windows.Forms.Panel()
         Me.pnl_bar = New System.Windows.Forms.Panel()
         Me.lbl_fraction = New System.Windows.Forms.Label()
+        Me.btn_set_zip = New System.Windows.Forms.Button()
+        Me.btn_set_rar = New System.Windows.Forms.Button()
+        Me.btn_set_7z = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.btn_out_7z = New System.Windows.Forms.Button()
+        Me.btn_out_rar = New System.Windows.Forms.Button()
+        Me.btn_out_zip = New System.Windows.Forms.Button()
+        Me.btn_out_folder = New System.Windows.Forms.Button()
+        Me.brn_convert = New System.Windows.Forms.Button()
+        Me.btn_clear = New System.Windows.Forms.Button()
+        Me.btn_remove = New System.Windows.Forms.Button()
         Me.pnl_bar_line.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -59,7 +70,7 @@ Partial Class Form1
         '
         Me.lbx_zip_files.FormattingEnabled = True
         Me.lbx_zip_files.HorizontalScrollbar = True
-        Me.lbx_zip_files.Location = New System.Drawing.Point(12, 142)
+        Me.lbx_zip_files.Location = New System.Drawing.Point(12, 171)
         Me.lbx_zip_files.Name = "lbx_zip_files"
         Me.lbx_zip_files.Size = New System.Drawing.Size(120, 95)
         Me.lbx_zip_files.TabIndex = 2
@@ -68,7 +79,7 @@ Partial Class Form1
         '
         Me.pnl_bar_line.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.pnl_bar_line.Controls.Add(Me.pnl_bar)
-        Me.pnl_bar_line.Location = New System.Drawing.Point(12, 243)
+        Me.pnl_bar_line.Location = New System.Drawing.Point(12, 272)
         Me.pnl_bar_line.Name = "pnl_bar_line"
         Me.pnl_bar_line.Size = New System.Drawing.Size(200, 50)
         Me.pnl_bar_line.TabIndex = 3
@@ -84,16 +95,130 @@ Partial Class Form1
         'lbl_fraction
         '
         Me.lbl_fraction.AutoSize = True
-        Me.lbl_fraction.Location = New System.Drawing.Point(12, 297)
+        Me.lbl_fraction.Location = New System.Drawing.Point(12, 326)
         Me.lbl_fraction.Name = "lbl_fraction"
         Me.lbl_fraction.Size = New System.Drawing.Size(0, 13)
         Me.lbl_fraction.TabIndex = 1
+        '
+        'btn_set_zip
+        '
+        Me.btn_set_zip.Location = New System.Drawing.Point(138, 12)
+        Me.btn_set_zip.Name = "btn_set_zip"
+        Me.btn_set_zip.Size = New System.Drawing.Size(53, 23)
+        Me.btn_set_zip.TabIndex = 5
+        Me.btn_set_zip.Text = "ZIP"
+        Me.btn_set_zip.UseVisualStyleBackColor = True
+        '
+        'btn_set_rar
+        '
+        Me.btn_set_rar.Location = New System.Drawing.Point(197, 12)
+        Me.btn_set_rar.Name = "btn_set_rar"
+        Me.btn_set_rar.Size = New System.Drawing.Size(53, 23)
+        Me.btn_set_rar.TabIndex = 6
+        Me.btn_set_rar.Text = "RAR"
+        Me.btn_set_rar.UseVisualStyleBackColor = True
+        '
+        'btn_set_7z
+        '
+        Me.btn_set_7z.Location = New System.Drawing.Point(256, 12)
+        Me.btn_set_7z.Name = "btn_set_7z"
+        Me.btn_set_7z.Size = New System.Drawing.Size(53, 23)
+        Me.btn_set_7z.TabIndex = 7
+        Me.btn_set_7z.Text = "7Z"
+        Me.btn_set_7z.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(315, 12)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(53, 23)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "No"
+        Me.Button4.UseVisualStyleBackColor = True
+        Me.Button4.Visible = False
+        '
+        'btn_out_7z
+        '
+        Me.btn_out_7z.Location = New System.Drawing.Point(256, 171)
+        Me.btn_out_7z.Name = "btn_out_7z"
+        Me.btn_out_7z.Size = New System.Drawing.Size(53, 23)
+        Me.btn_out_7z.TabIndex = 11
+        Me.btn_out_7z.Text = "7Z"
+        Me.btn_out_7z.UseVisualStyleBackColor = True
+        Me.btn_out_7z.Visible = False
+        '
+        'btn_out_rar
+        '
+        Me.btn_out_rar.Location = New System.Drawing.Point(197, 171)
+        Me.btn_out_rar.Name = "btn_out_rar"
+        Me.btn_out_rar.Size = New System.Drawing.Size(53, 23)
+        Me.btn_out_rar.TabIndex = 10
+        Me.btn_out_rar.Text = "RAR"
+        Me.btn_out_rar.UseVisualStyleBackColor = True
+        Me.btn_out_rar.Visible = False
+        '
+        'btn_out_zip
+        '
+        Me.btn_out_zip.Location = New System.Drawing.Point(138, 171)
+        Me.btn_out_zip.Name = "btn_out_zip"
+        Me.btn_out_zip.Size = New System.Drawing.Size(53, 23)
+        Me.btn_out_zip.TabIndex = 9
+        Me.btn_out_zip.Text = "ZIP"
+        Me.btn_out_zip.UseVisualStyleBackColor = True
+        Me.btn_out_zip.Visible = False
+        '
+        'btn_out_folder
+        '
+        Me.btn_out_folder.Location = New System.Drawing.Point(315, 171)
+        Me.btn_out_folder.Name = "btn_out_folder"
+        Me.btn_out_folder.Size = New System.Drawing.Size(53, 23)
+        Me.btn_out_folder.TabIndex = 12
+        Me.btn_out_folder.Text = "Folder"
+        Me.btn_out_folder.UseVisualStyleBackColor = True
+        '
+        'brn_convert
+        '
+        Me.brn_convert.Location = New System.Drawing.Point(12, 142)
+        Me.brn_convert.Name = "brn_convert"
+        Me.brn_convert.Size = New System.Drawing.Size(120, 23)
+        Me.brn_convert.TabIndex = 13
+        Me.brn_convert.Text = "CONVERT"
+        Me.brn_convert.UseVisualStyleBackColor = True
+        '
+        'btn_clear
+        '
+        Me.btn_clear.Location = New System.Drawing.Point(138, 41)
+        Me.btn_clear.Name = "btn_clear"
+        Me.btn_clear.Size = New System.Drawing.Size(120, 23)
+        Me.btn_clear.TabIndex = 14
+        Me.btn_clear.Text = "CLEAR"
+        Me.btn_clear.UseVisualStyleBackColor = True
+        '
+        'btn_remove
+        '
+        Me.btn_remove.Location = New System.Drawing.Point(138, 70)
+        Me.btn_remove.Name = "btn_remove"
+        Me.btn_remove.Size = New System.Drawing.Size(120, 23)
+        Me.btn_remove.TabIndex = 15
+        Me.btn_remove.Text = "REMOVE"
+        Me.btn_remove.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(666, 468)
+        Me.Controls.Add(Me.btn_remove)
+        Me.Controls.Add(Me.btn_clear)
+        Me.Controls.Add(Me.brn_convert)
+        Me.Controls.Add(Me.btn_out_folder)
+        Me.Controls.Add(Me.btn_out_7z)
+        Me.Controls.Add(Me.btn_out_rar)
+        Me.Controls.Add(Me.btn_out_zip)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.btn_set_7z)
+        Me.Controls.Add(Me.btn_set_rar)
+        Me.Controls.Add(Me.btn_set_zip)
         Me.Controls.Add(Me.lbl_fraction)
         Me.Controls.Add(Me.pnl_bar_line)
         Me.Controls.Add(Me.lbx_zip_files)
@@ -114,4 +239,15 @@ Partial Class Form1
     Friend WithEvents pnl_bar_line As Panel
     Friend WithEvents pnl_bar As Panel
     Friend WithEvents lbl_fraction As Label
+    Friend WithEvents btn_set_zip As Button
+    Friend WithEvents btn_set_rar As Button
+    Friend WithEvents btn_set_7z As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents btn_out_7z As Button
+    Friend WithEvents btn_out_rar As Button
+    Friend WithEvents btn_out_zip As Button
+    Friend WithEvents btn_out_folder As Button
+    Friend WithEvents brn_convert As Button
+    Friend WithEvents btn_clear As Button
+    Friend WithEvents btn_remove As Button
 End Class
--- bulk zip extractor/Form1.vb
+++ bulk zip extractor/Form1.vb
@@ -1,14 +1,50 @@
 ﻿Imports System.IO
 Imports System.IO.Compression
 Imports Shell32
+Imports SharpCompress.Archive
+Imports SharpCompress.Common
 
 Public Class Form1
     Dim i As Integer = 0
+    Dim file As String
+    Dim file_count As Integer = 0
+    Dim B_Set_7Z As Boolean = False
+    Dim B_Set_Zip As Boolean = True
+    Dim B_Set_Rar As Boolean = False
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        btn_set_zip.BackColor = Color.Green
+        btn_set_rar.BackColor = Color.Red
+        btn_set_7z.BackColor = Color.Red
+
+        btn_out_folder.BackColor = Color.Green
+        btn_out_rar.BackColor = Color.Red
+        btn_out_7z.BackColor = Color.Red
+        btn_out_zip.BackColor = Color.Red
+
+        btn_out_folder.Location = btn_out_zip.Location
+        btn_out_folder.Visible = False
+    End Sub
 
     Private Sub btn_open_Click(sender As Object, e As EventArgs) Handles btn_open.Click
+        If B_Set_7Z = True Then
+            OFD_Zip.Filter = "7Z Files|*7Z"
+        End If
+
+        If B_Set_Zip = True Then
+            OFD_Zip.Filter = "Zip Files|*Zip"
+        End If
+
+        If B_Set_Rar = True Then
+            OFD_Zip.Filter = "Rar Files|*Rar"
+        End If
+
+        btn_set_7z.Enabled = False
+        btn_set_rar.Enabled = False
+        btn_set_zip.Enabled = False
+
         If OFD_Zip.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
-            Dim file As String
-            Dim file_count As Integer = 0
 
             For Each file In OFD_Zip.FileNames
                 file_count += 1
@@ -19,38 +55,218 @@ Public Class Form1
 
             For Each file In OFD_Zip.FileNames
                 lbx_zip_paths.Items.Add(file)
-
-                Dim fileinfo As Array
-                fileinfo = Split(file, "\")
-                Dim file_number = fileinfo.Length
-                Dim filename As String = (fileinfo(file_number - 1))
-                filename = filename.Replace(".zip", "")
-
-                lbx_zip_files.Items.Add(filename)
-
-                Dim folder_make As String = file
-                folder_make = folder_make.Replace(".zip", "")
-
-                Dim sc As New Shell32.Shell()
-                'Create directory in which you will unzip your files .
-                System.IO.Directory.CreateDirectory(folder_make)
-                IO.Directory.CreateDirectory(folder_make)
-                'Declare the folder where the files will be extracted
-                Dim output As Shell32.Folder = sc.NameSpace(folder_make)
-                'Declare your input zip file as folder  .
-                Dim input As Shell32.Folder = sc.NameSpace(file)
-                'Extract the files from the zip file using the CopyHere command .
-                output.CopyHere(input.Items, 4)
-
-                i += 1
-
-                lbl_fraction.Text = i & " out of " & file_count
-                pnl_bar.Width = (i / file_count) * 100 * 2
             Next
+
+        Else
+
+            btn_set_7z.Enabled = True
+            btn_set_rar.Enabled = True
+            btn_set_zip.Enabled = True
+
         End If
     End Sub
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Private Sub btn_set_7z_Click(sender As Object, e As EventArgs) Handles btn_set_7z.Click
+        btn_set_7z.BackColor = Color.Green
+        btn_set_zip.BackColor = Color.Red
+        btn_set_rar.BackColor = Color.Red
 
+        B_Set_7Z = True
+        B_Set_Zip = False
+        B_Set_Rar = False
+    End Sub
+
+    Private Sub btn_set_rar_Click(sender As Object, e As EventArgs) Handles btn_set_rar.Click
+        btn_set_rar.BackColor = Color.Green
+        btn_set_zip.BackColor = Color.Red
+        btn_set_7z.BackColor = Color.Red
+
+        B_Set_7Z = False
+        B_Set_Zip = False
+        B_Set_Rar = True
+    End Sub
+
+    Private Sub btn_set_zip_Click(sender As Object, e As EventArgs) Handles btn_set_zip.Click
+        btn_set_zip.BackColor = Color.Green
+        btn_set_rar.BackColor = Color.Red
+        btn_set_7z.BackColor = Color.Red
+
+        B_Set_7Z = False
+        B_Set_Zip = True
+        B_Set_Rar = False
+    End Sub
+
+    Private Sub btn_out_7z_Click(sender As Object, e As EventArgs) Handles btn_out_7z.Click
+        btn_out_7z.BackColor = Color.Green
+        btn_out_zip.BackColor = Color.Red
+        btn_out_rar.BackColor = Color.Red
+        btn_out_folder.BackColor = Color.Red
+    End Sub
+
+    Private Sub btn_out_rar_Click(sender As Object, e As EventArgs) Handles btn_out_rar.Click
+        btn_out_rar.BackColor = Color.Green
+        btn_out_zip.BackColor = Color.Red
+        btn_out_7z.BackColor = Color.Red
+        btn_out_folder.BackColor = Color.Red
+    End Sub
+
+    Private Sub btn_out_zip_Click(sender As Object, e As EventArgs) Handles btn_out_zip.Click
+        btn_out_zip.BackColor = Color.Green
+        btn_out_rar.BackColor = Color.Red
+        btn_out_7z.BackColor = Color.Red
+        btn_out_folder.BackColor = Color.Red
+    End Sub
+
+    Private Sub btn_out_folder_Click(sender As Object, e As EventArgs) Handles btn_out_folder.Click
+        btn_out_folder.BackColor = Color.Green
+        btn_out_zip.BackColor = Color.Red
+        btn_out_rar.BackColor = Color.Red
+        btn_out_7z.BackColor = Color.Red
+    End Sub
+
+    Private Sub brn_convert_Click(sender As Object, e As EventArgs) Handles brn_convert.Click
+
+        If B_Set_7Z = True Then
+            Set_7Z_Click(sender)
+        End If
+
+        If B_Set_Zip = True Then
+            Set_Zip_Click(sender)
+        End If
+
+        If B_Set_Rar = True Then
+            Set_Rar_Click(sender)
+        End If
+
+        'For Each file In OFD_Zip.FileNames
+
+        '    Dim fileinfo As Array
+        '    fileinfo = Split(file, "\")
+        '    Dim file_number = fileinfo.Length
+        '    Dim filename As String = (fileinfo(file_number - 1))
+        '    filename = filename.Replace(".zip", "")
+
+        '    lbx_zip_files.Items.Add(filename)
+
+        '    Dim folder_make As String = file
+        '    folder_make = folder_make.Replace(".zip", "")
+
+        '    Dim sc As New Shell32.Shell()
+        '    'Create directory in which you will unzip your files .
+        '    System.IO.Directory.CreateDirectory(folder_make)
+        '    IO.Directory.CreateDirectory(folder_make)
+        '    'Declare the folder where the files will be extracted
+        '    Dim output As Shell32.Folder = sc.NameSpace(folder_make)
+        '    'Declare your input zip file as folder  .
+        '    Dim input As Shell32.Folder = sc.NameSpace(file)
+        '    'Extract the files from the zip file using the CopyHere command .
+        '    output.CopyHere(input.Items, 4)
+
+        '    i += 1
+
+        '    lbl_fraction.Text = i & " out of " & file_count
+        '    pnl_bar.Width = (i / file_count) * 100 * 2
+        'Next
+    End Sub
+
+    Private Sub Set_Zip_Click(butt As Button)
+        For Each file In OFD_Zip.FileNames
+
+            Dim fileinfo As Array
+            fileinfo = Split(file, "\")
+            Dim file_number = fileinfo.Length
+            Dim filename As String = (fileinfo(file_number - 1))
+            filename = filename.Replace(".zip", "")
+
+            lbx_zip_files.Items.Add(filename)
+
+            Dim folder_make As String = file
+            folder_make = folder_make.Replace(".zip", "")
+
+            Dim sc As New Shell32.Shell()
+            System.IO.Directory.CreateDirectory(folder_make)
+            IO.Directory.CreateDirectory(folder_make)
+            Dim output As Shell32.Folder = sc.NameSpace(folder_make)
+            Dim input As Shell32.Folder = sc.NameSpace(file)
+            output.CopyHere(input.Items, 4)
+
+            i += 1
+
+            lbl_fraction.Text = i & " out of " & file_count
+            pnl_bar.Width = (i / file_count) * 100 * 2
+        Next
+    End Sub
+
+    Private Sub Set_Rar_Click(butt As Button)
+        For Each file In OFD_Zip.FileNames
+
+            Dim fileinfo As Array
+            fileinfo = Split(file, "\")
+            Dim file_number = fileinfo.Length
+            Dim filename As String = (fileinfo(file_number - 1))
+            filename = filename.Replace(".rar", "")
+
+            lbx_zip_files.Items.Add(filename)
+
+            Dim folder_make As String = file
+            folder_make = folder_make.Replace(".rar", "")
+
+            Dim archive As IArchive = ArchiveFactory.Open(file)
+            For Each entry In archive.Entries
+                If Not entry.IsDirectory Then
+                    Console.WriteLine(entry.FilePath)
+                    IO.Directory.CreateDirectory(folder_make)
+                    entry.WriteToDirectory(folder_make,
+          ExtractOptions.ExtractFullPath Or ExtractOptions.Overwrite)
+                End If
+            Next
+
+            i += 1
+
+            lbl_fraction.Text = i & " out of " & file_count
+            pnl_bar.Width = (i / file_count) * 100 * 2
+        Next
+    End Sub
+
+    Private Sub Set_7Z_Click(butt As Button)
+        For Each file In OFD_Zip.FileNames
+
+            Dim fileinfo As Array
+            fileinfo = Split(file, "\")
+            Dim file_number = fileinfo.Length
+            Dim filename As String = (fileinfo(file_number - 1))
+            filename = filename.Replace(".7z", "")
+
+            lbx_zip_files.Items.Add(filename)
+
+            Dim folder_make As String = file
+            folder_make = folder_make.Replace(".7z", "")
+
+            Dim archive As IArchive = ArchiveFactory.Open(file)
+            For Each entry In archive.Entries
+                If Not entry.IsDirectory Then
+                    Console.WriteLine(entry.FilePath)
+                    IO.Directory.CreateDirectory(folder_make)
+                    entry.WriteToDirectory(folder_make,
+          ExtractOptions.ExtractFullPath Or ExtractOptions.Overwrite)
+                End If
+            Next
+
+            i += 1
+
+            lbl_fraction.Text = i & " out of " & file_count
+            pnl_bar.Width = (i / file_count) * 100 * 2
+        Next
+    End Sub
+
+    Private Sub btn_remove_Click(sender As Object, e As EventArgs) Handles btn_remove.Click
+        Dim remove_file As String
+
+        remove_file = lbx_zip_paths.SelectedItem
+        lbx_zip_paths.Items.Remove(remove_file)
+    End Sub
+
+    Private Sub btn_clear_Click(sender As Object, e As EventArgs) Handles btn_clear.Click
+        lbx_zip_paths.Items.Clear()
     End Sub
 End Class
